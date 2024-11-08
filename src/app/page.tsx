ed0aefd5c7ab8ad613a62b4fa8a133c8b283e6ee
vitoralveschaves <vitorchavesalv@gmail.com>
2024-11-08 17:15:00 -0300

    feat: add quick search component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 'use client'
 
+import { QuickSearch } from "./components/quick-search"
 import { TripSearch } from "./components/trip-search"
 
 export default function Home() {
   return (
     <div>
       <TripSearch />
+      <QuickSearch />
     </div>
   )
 }
