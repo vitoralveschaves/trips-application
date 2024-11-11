a341459f980537ccede6d1ca35ce1a5309650e80
vitoralveschaves <vitorchavesalv@gmail.com>
2024-11-11 10:12:48 -0300

    feat: add date validation

--- src/app/trips/[tripId]/components/trip-reservation.tsx
+++ src/app/trips/[tripId]/components/trip-reservation.tsx
@@ -7,6 +7,7 @@ import { differenceInDays } from "date-fns"
 import { Controller, useForm } from "react-hook-form"
 
 interface TripReservationProps {
+  tripId: string
   tripStartDate: Date
   tripEndDate: Date
   maxGuests: number
@@ -19,12 +20,47 @@ interface TripReservationForm {
   endDate: Date | null
 }
 
-export const TripReservation = ({ tripStartDate, tripEndDate, maxGuests, pricePerDay }: TripReservationProps) => {
+export const TripReservation = ({ tripId, tripStartDate, tripEndDate, maxGuests, pricePerDay }: TripReservationProps) => {
 
-  const { register, handleSubmit, control, watch, formState: { errors } } = useForm<TripReservationForm>();
+  const { register, handleSubmit, control, watch, setError, formState: { errors } } = useForm<TripReservationForm>();
 
-  const onSubmit = (data: any) => {
-    console.log(data)
+  const onSubmit = async (data: TripReservationForm) => {
+    const response = await fetch("http://localhost:3000/api/trips/check", {
+      method: 'POST',
+      body: Buffer.from(JSON.stringify({
+        startDate: data.startDate,
+        endDate: data.endDate,
+        tripId
+      }))
+    })
+
+    const res = await response.json();
+
+    if (res?.error?.code === 'TRIP_ALREADY_RESERVED') {
+      setError('startDate', {
+        type: 'manual',
+        message: 'Esta data já está reservada.'
+      });
+
+      setError('endDate', {
+        type: 'manual',
+        message: 'Esta data já está reservada.'
+      });
+    }
+
+    if (res?.error?.code === "INVALID_START_DATE") {
+      return setError("startDate", {
+        type: "manual",
+        message: "Data inválida.",
+      });
+    }
+
+    if (res?.error?.code === "INVALID_END_DATE") {
+      return setError("endDate", {
+        type: "manual",
+        message: "Data inválida.",
+      });
+    }
   }
 
   const startDate = watch('startDate');
