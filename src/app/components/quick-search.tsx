aa0ef04e0d82d521288d21dc19b748d0ef13af2e
vitoralveschaves <vitorchavesalv@gmail.com>
2024-11-12 12:57:12 -0300

    feat: add quick search

--- src/app/components/quick-search.tsx
+++ src/app/components/quick-search.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Link from "next/link"
 
 export const QuickSearch = () => {
   return (
@@ -10,19 +11,27 @@ export const QuickSearch = () => {
       </div>
       <div className="flex w-full justify-between mt-5">
         <div className="flex flex-col items-center gap-1">
-          <Image width={35} height={35} src={'/hotel-icon.png'} alt="hotel" />
+          <Link href={"/trips/search?text=hotel"}>
+            <Image width={35} height={35} src={'/hotel-icon.png'} alt="hotel" />
+          </Link>
           <p className="text-sm text-grayPrimary">Hotel</p>
         </div>
         <div className="flex flex-col items-center gap-1">
-          <Image width={35} height={35} src={'/farm-icon.png'} alt="hotel" />
+          <Link href={"/trips/search?text=fazenda"}>
+            <Image width={35} height={35} src={'/farm-icon.png'} alt="farm" />
+          </Link>
           <p className="text-sm text-grayPrimary">Fazenda</p>
         </div>
         <div className="flex flex-col items-center gap-1">
-          <Image width={35} height={35} src={'/cottage-icon.png'} alt="hotel" />
+          <Link href={"/trips/search?text=chalé"}>
+            <Image width={35} height={35} src={'/cottage-icon.png'} alt="cottage" />
+          </Link>
           <p className="text-sm text-grayPrimary">Chalé</p>
         </div>
         <div className="flex flex-col items-center gap-1">
-          <Image width={35} height={35} src={'/inn-icon.png'} alt="hotel" />
+          <Link href={"/trips/search?text=pousada"}>
+            <Image width={35} height={35} src={'/inn-icon.png'} alt="inn" />
+          </Link>
           <p className="text-sm text-grayPrimary">Pousada</p>
         </div>
       </div>
